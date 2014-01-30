e1dbe70d96eb370ddbe7f1c3f502b14a9ac00a4e
Sean Adkinson <sean.adkinson@gmail.com>
2014-01-29 22:44:43 -0800

    Only queueing animations, not calculations and executions

--- robot.js
+++ robot.js
@@ -3,7 +3,7 @@ $(function() {
     room.create();
 });
 
-var queue = []
+var currentAnimation;
 
 var room = {
 
@@ -49,18 +49,17 @@ var robot = {
     el:null,
 
     turnRight: function() {
-        this.el.animateRotate(90, runQueue)
+        this.el.animateRotate(90)
     },
 
     turnLeft: function() {
-        this.el.animateRotate(-90, runQueue)
+        this.el.animateRotate(-90)
     },
 
     move: function(n) {
         var x = this.el.data('x');
         var y = this.el.data('y');
         var dir = this.el.data('dir');
-//        console.log("Moving " + n + " space from " + x + ", " + y + ", dir: " + dir);
 
         if (dir == 'u') {
             y -= n;
@@ -76,27 +75,25 @@ var robot = {
         }
 
         if (x < 0 || y < 0 || x >= room.dim || y >= room.dim) {
-            queue.length = 0;
             alert("Out of range!");
         }
         else {
-            this.el.toTile(x, y, function() {
-                robot.checkClean();
-                runQueue();
-            });
+            this.el.toTile(x, y);
+            this.checkClean();
         }
     },
 
     checkClean: function() {
         var x = this.el.data('x');
         var y = this.el.data('y');
-//        console.log("Anything at " + x + ", " + y + "?");
+//        console.log("Trash at " + x + ", " + y + "?");
 
-        $('.trash').each(function(i, t) {
+        var takeOut = $('.trash').filter(function(i, t) {
             t = $(t);
-            if (t.data('x') == x && t.data('y') == y) {
-                t.remove();
-            }
+            return t.data('x') == x && t.data('y') == y;
+        });
+        $.when(currentAnimation).done(function() {
+            takeOut.fadeOut();
         });
     }
 }
@@ -115,47 +112,17 @@ function forward(n) {
 }
 
 function turnRight() {
-    var kickoff = !queue.length;
-    queue.push(function() {
-//        console.log("Turning Right");
-        robot.turnRight();
-    });
-    if (kickoff) {
-        runQueue();
-    }
+    robot.turnRight();
 }
 
 function turnLeft() {
-    var kickoff = !queue.length;
-    queue.push(function() {
-//        console.log("Turning Left");
-        robot.turnLeft();
-    });
-    if (kickoff) {
-        runQueue();
-    }
+    robot.turnLeft();
 }
 
 function move(n) {
-    var kickoff = !queue.length;
-
     var d = n < 0 ? -1 : 1;
     for (var i=0; i<Math.abs(n); i++) {
-        queue.push(function() {
-//            console.log("Moving: " + d);
-            robot.move(d);
-        });
-    }
-    if (kickoff) {
-        runQueue();
-    }
-}
-
-
-function runQueue() {
-    if (queue.length) {
-//        console.log("Dequeue: " + queue.length);
-        queue.splice(0, 1)[0]();
+        robot.move(d);
     }
 }
 
@@ -165,6 +132,20 @@ $.fn.animateRotate = function(angle, complete) {
     var to = from + angle;
     this.data('degs', to);
 
+    var n = (to + 360) % 360;
+    if (n == 0) {
+        this.data('dir', 'u');
+    }
+    if (n == 180) {
+        this.data('dir', 'd');
+    }
+    if (n == 270) {
+        this.data('dir', 'l');
+    }
+    if (n == 90) {
+        this.data('dir', 'r');
+    }
+
     return this.animate({
         deg: to
     }, {
@@ -172,44 +153,22 @@ $.fn.animateRotate = function(angle, complete) {
             $.style(this, 'transform', 'rotate(' + now + 'deg)');
         },
         done: function() {
-//            console.log("Rotate DONE");
-            var n = (to + 360) % 360;
-            if (n == 0) {
-                $(this).data('dir', 'u');
-            }
-            if (n == 180) {
-                $(this).data('dir', 'd');
-            }
-            if (n == 270) {
-                $(this).data('dir', 'l');
-            }
-            if (n == 90) {
-                $(this).data('dir', 'r');
-            }
             complete && complete();
         }
     });
-
-//    return this.each(function(i, e) {
-//        args.step = function(now) {
-//            $.style(e, 'transform', 'rotate(' + now + 'deg)');
-//            if (step) return step.apply(this, arguments);
-//        };
-//
-//        $({deg: from}).animate({deg: to}, args);
-//    });
 };
 
 $.fn.toTile = function(x, y, complete) {
+    this.data('x', x);
+    this.data('y', y);
+    var d = currentAnimation = $.Deferred();
     return this.animate({
         top: 7+y*(room.size+1),
         left: 7+x*(room.size+1)
     }, {
-        done: $.proxy(function(x, y) {
-//            console.log("Move DONE");
-            $(this).data('x', x);
-            $(this).data('y', y);
+        done: function (){
+            d.resolve();
             complete && complete();
-        }, this, x, y)
+        }
     });
 };
